45471357e90bd53c741fdcd055152dbaf7a86bd9
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-27 16:18:50 +0530

    final commit3

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -42,7 +42,7 @@ function LoginPage({ setShowLoginPage }) {
     }
   
     try {
-      const response = await axios.post('https://kick-off-api.onrender.com', login);
+      const response = await axios.post('https://kick-off-api.onrender.com/auth/login', login);
       if (response?.data?.login) {
         localStorage.setItem('token', response?.data?.token);
         dispatch(setuser(response?.data?.user));
