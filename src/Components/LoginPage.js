dc615f4614298b8ccde76bd8a67d204c1b64d502
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-03-28 12:11:49 +0530

    cors update

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -27,20 +27,20 @@ function LoginPage({ setShowLoginPage }) {
   const [showPassword, setShowPassword] = useState(false); // State to control password visibility
   const navigate = useNavigate();
 
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
 
   const handleLogin = async () => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-  
+
     const emailValid = emailRegex.test(login.email);
     const passwordValid = passwordRegex.test(login.password);
-  
+
     if (!emailValid || !passwordValid) {
       setMessage('Invalid email or password. Please check your input.');
       return;
     }
-  
+
     try {
       const response = await axios.post('https://kick-off-api.onrender.com/auth/login', login);
       if (response?.data?.login) {
@@ -51,11 +51,11 @@ function LoginPage({ setShowLoginPage }) {
         setMessage('Login failed. Please check your credentials.');
       }
     } catch (error) {
-      setMessage('Login failed. Please check your credentials.');
+       setMessage('Login failed. Please check your credentials.');
       console.error('Login error:', error);
     }
   };
-  
+
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -68,10 +68,10 @@ function LoginPage({ setShowLoginPage }) {
           <MDBCard className='bg-dark text-white mx-auto' style={{ borderRadius: '6rem', maxWidth: '400px', maxHeight: '800px', marginTop: '100px' }}>
             <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
               <h2 className="fw-bold mb-2 text-uppercase">KICK OFF
-</h2>
+              </h2>
 
-            {/* <h2>ggggggggggggggggg</h2> */}
-            <br></br>
+              {/* <h2>ggggggggggggggggg</h2> */}
+              <br></br>
               {message && <p className="text-danger">{message}</p>}
               <MDBInput
                 wrapperClass='mb-4 mx-5 w-100'
@@ -79,7 +79,7 @@ function LoginPage({ setShowLoginPage }) {
                 label='Email'
                 id='form3'
                 type='email'
-                onChange={(e) => setlogin({...login, email: e.target.value })}
+                onChange={(e) => setlogin({ ...login, email: e.target.value })}
               />
               <MDBInput
                 wrapperClass='mb-4 mx-5 w-100'
@@ -96,7 +96,7 @@ function LoginPage({ setShowLoginPage }) {
                 {showPassword ? 'Hide Password' : 'Show Password'}
               </button>
 
-              <button className='w-50 btn btn-dark' onClick ={handleLogin} style={{ marginLeft: '10px' }}>LOGIN</button>
+              <button className='w-50 btn btn-dark' onClick={handleLogin} style={{ marginLeft: '10px' }}>LOGIN</button>
               <div>
                 <p className="mb-0">Don't have an account? <a href="#" className="text-white-50 fw-bold" onClick={() => setShowLoginPage("signup")} style={{ color: "blue" }}>Sign Up</a></p>
               </div>
