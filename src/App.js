94cfeaf4ec0057af0bd28ac10dc6f30fcca6b154
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-04-06 13:16:57 +0530

    not found update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarMain from "./Pages/NavbarMain";
+import WelcomePage from "./Pages/WelcomePage";
 
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from "./Components/LoginPage";
 import Home from "./Pages/Home";
 import CourtReg from "./Pages/CourtReg";
 import MyCourts from "./Pages/MyCourts";
 import SingleCourtPage from "./Pages/SingleCourtPage";
 import UserCourtPage from "./Pages/UserCourtPage";
-import MyBookings from "./Pages/MyBookings";
-import WelcomePage from "./Pages/WelcomePage";
-import NavbarMain from "./Pages/NavbarMain";
-
 import { Authorization, LoginpageAuth } from "./Authorization/Authorization";
+import MyBookings from "./Pages/MyBookings";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+
           <Route element={<LoginpageAuth />}>
             <Route path="/" element={<WelcomePage />} />
           </Route>
           <Route element={<Authorization/>}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/CourtRegistration" element={<CourtReg />} />
-            <Route path="/MyCourts" element={<MyCourts />} />
-            <Route path="/openCourtEdit/:id" element={<SingleCourtPage />} />
-            <Route path="/courtBooking/:id" element={<UserCourtPage />} />
-            <Route path="/MyBookings" element={<MyBookings/>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/CourtRegistration" element={<CourtReg />} />
+          <Route path="/MyCourts" element={<MyCourts />} />
+          <Route path="/openCourtEdit/:id" element={<SingleCourtPage />} />
+          <Route path="/courtBooking/:id" element={<UserCourtPage />} />
+          <Route path="/MyBookings" element={<MyBookings/>} />
+          
+
+
           </Route>
         </Routes>
+
       </BrowserRouter>
+
     </div>
   );
 }
