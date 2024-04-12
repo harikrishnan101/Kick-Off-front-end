d9fe64880ad323f3e5d0e9b1c02070479413821b
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-04-12 20:29:26 +0530

    image 4

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -8,12 +8,11 @@ function Cards({ data }) {
 
   return (
     <Card style={{ width: '18rem' }} onClick={() => navigate(`/courtBooking/${data._id}`)}>
-      <img
-        variant="top"
-        src={`${BASEURL}/CourtsImg/${data?.image}`} // Assuming the images are stored in the 'CourtsImg' directory
-        style={{ height: '200px', objectFit: 'cover', width: '100%' }} // Add width: '100%' to ensure the image fills the card width
-        alt={data.name} // Provide an alt attribute for accessibility
-      />
+     <img
+  src={`${BASEURL}/CourtsImg/${data?.image}`} // Assuming the images are stored in the 'CourtsImg' directory
+  style={{ height: '200px', objectFit: 'cover' }}
+/>
+
       <Card.Body>
         <Card.Title>{data?.name}</Card.Title>
         <Card.Text>{data?.location}</Card.Text>
