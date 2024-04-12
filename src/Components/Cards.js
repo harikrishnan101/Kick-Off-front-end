17e77e19873e1904d8b4982197c5adf0dc342605
HARIKRISHNAN R <harikrishnan183hari@gmail.com>
2024-04-12 13:43:17 +0530

    image 1

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,18 +1,23 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { BASEURL } from '../Constant/baseUrl';
 import { useNavigate } from 'react-router-dom';
 
-function Cards({data}) {
-  
-  const navigate=useNavigate()
+function Cards({ data }) {
+  const navigate = useNavigate();
+
   return (
-    <Card style={{ width: '18rem' }} onClick={() => navigate(`/courtBooking/${data._id}`)} >
-      <Card.Img variant="top" src={`${BASEURL}/CourtsImg/${data?.image}`}   style={{ height: '200px', objectFit: 'cover' }}  />
+    <Card style={{ width: '18rem' }} onClick={() => navigate(`/courtBooking/${data._id}`)}>
+      {data.image && (
+        <Card.Img
+          variant="top"
+          src={`${BASEURL}/Courts/${data.image}`} // Assuming the images are stored in the 'Courts' directory
+          style={{ height: '200px', objectFit: 'cover' }}
+        />
+      )}
       <Card.Body>
-        <Card.Title>{data?.name}</Card.Title>
-        <Card.Title>{data?.location}</Card.Title>
-        
+        <Card.Title>{data.name}</Card.Title>
+        <Card.Title>{data.location}</Card.Title>
       </Card.Body>
     </Card>
   );
